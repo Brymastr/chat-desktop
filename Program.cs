a27abcb4c076f974dfc428fea48141a9f3951d5b
Brycen <dorsay@live.ca>
2017-04-18 14:32:04 -0700

    Receive and send messages

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net.Http;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
@@ -8,31 +7,66 @@ using Console = Colorful.Console;
 
 namespace chat
 {
-    class Program
+  class Program
     {
+        const int MESSAGE_SIZE = 1024;
         static void Main(string[] args)
         {
-            Task.Run(() => Try());
+            MainAsync().Wait();
         }
-        // static async Task MainAsync()
-        // {
-        //     Console.WriteAscii("Hello World!");
-        //     var socket = new ClientWebSocket();
-        //     var uri = new Uri("ws://localhost:9000");
-
-        //     await socket.ConnectAsync(uri, CancellationToken.None);
-
-        //     var message = "This is a message";
-
-        //     var buffer = new ArraySegment<Byte>(Encoding.UTF8.GetBytes(message));
-
-        //     return await socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
-        // }
-
-        static async Task<String> Try()
+        static async Task MainAsync()
         {
-            using (HttpClient client = new HttpClient())
-                return await client.GetStringAsync("http://localhost:9000");
+            var uri = new Uri("ws://localhost:9000/", UriKind.Absolute);
+            
+            var socket = new ClientWebSocket();
+            await socket.ConnectAsync(uri, CancellationToken.None);
+
+            await Task.WhenAll(ReceiveMessages(socket), SendMessages(socket));
         }
+
+        static async Task SendMessages(ClientWebSocket socket)
+        {
+            while(true)
+            {
+                Console.Write("> ");
+                var input = Console.ReadLine();
+                await SendMessage(socket, input);
+            }
+        }
+
+        static async Task SendMessage(ClientWebSocket socket, String message)
+        {
+            await socket.SendAsync(
+                new ArraySegment<Byte>(Encoding.UTF8.GetBytes(message)),
+                WebSocketMessageType.Text,
+                true,
+                CancellationToken.None
+            );
+        }
+
+        static async Task ReceiveMessages(ClientWebSocket socket)
+        {
+            while(true)
+            {
+                ArraySegment<Byte> buffer = new ArraySegment<Byte>(new Byte[MESSAGE_SIZE]);
+                await socket.ReceiveAsync(buffer, CancellationToken.None);
+
+                Console.WriteLine(TrimResponse(buffer));
+            }
+        }
+
+        static String TrimResponse(ArraySegment<Byte> buffer)
+        {
+            var lastByte = MESSAGE_SIZE - 1;
+            while(buffer.Array[lastByte - 1] == 0)
+                lastByte--;
+            var messageArray = new Byte[lastByte];
+            Array.Copy(buffer.Array, messageArray, lastByte);
+
+            var message = Encoding.UTF8.GetString(messageArray);
+
+            return message;
+        }
+
     }
 }
